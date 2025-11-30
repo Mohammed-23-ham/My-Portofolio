c652a7614af71aef8617d3f51ab313c187cd28f1
ABN HALLOMAH <alhamawymohamed@gmail.com>
2025-11-30 12:34:38 +0300

    fix3

--- src/Componants/Hero/Hero.jsx
+++ src/Componants/Hero/Hero.jsx
@@ -24,10 +24,10 @@ export default function Hero({ id }) {
         <p>I'm Mohammed AL-Hamawy, a Software Designer from Syria.
            I was born in 2007. I'm studying at Homs University, Department at Softwar Engineering and I'm still improving my skills. </p>
         <div className="icons flex">
-          <a href='https://www.facebook.com/profile.php?id=61560922022150' className="icon icon-facebook2"></a>
-          <a href='https://www.instagram.com/mohammedalhamawy?igsh=MWdkdXQ4dnUxN2Rldg==' className="icon icon-instagram"></a>
-          <a href='https://github.com/Mohammed-23-ham' className="icon icon-github"></a>
-          <a href='https://linkedin.com/in/mohammed-alhamawy-602926342' className="icon icon-linkedin-square"></a>
+          <a target='_blanck' href='https://www.facebook.com/profile.php?id=61560922022150' className="icon icon-facebook2"></a>
+          <a target='_blanck' href='https://www.instagram.com/mohammedalhamawy?igsh=MWdkdXQ4dnUxN2Rldg==' className="icon icon-instagram"></a>
+          <a target='_blanck' href='https://github.com/Mohammed-23-ham' className="icon icon-github"></a>
+          <a target='_blanck' href='https://linkedin.com/in/mohammed-alhamawy-602926342' className="icon icon-linkedin-square"></a>
         </div>
       </div>
       <div className="rsection animation">{aniLap ? <Lottie animationData={aniLap} /> : null}</div>
